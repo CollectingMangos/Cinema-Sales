828b5599370cda12291eb1d5150f24895531b731
Ruben <rubenfds@icloud.com>
2025-05-16 14:23:29 +0200

    Added functions, completed get movies

--- server/server.py
+++ server/server.py
@@ -20,6 +20,66 @@ server.listen()
 print(f'[STARTING] Listening on server: {SERVER} & port: {PORT}')
 logging.info(f'Server is now listening on {SERVER}:{PORT}')
 
+def handle_requests(request):
+    pass
+
+def get_movies():
+    try:
+        connection = sqlite3.connect('cinema.db')
+        cursor = connection.cursor()
+        cursor.execute('SELECT * FROM movies')
+        movies = cursor.fetchall()
+        return {'status':'success', 'movies': movies}
+    except:
+        return {'status':'error', 'message': 'Failed to fetch movies'}
+    finally:
+        connection.close()
+
+def add_movie():
+    try:
+        connection = sqlite3.connect('cinema.db')
+        cursor = connection.cursor()
+    except:
+        pass
+    finally:
+        connection.close()
+
+def update_movie():
+    try:
+        connection = sqlite3.connect('cinema.db')
+        cursor = connection.cursor()
+    except:
+        pass
+    finally:
+        connection.close()
+
+def delete_movie():
+    try:
+        connection = sqlite3.connect('cinema.db')
+        cursor = connection.cursor()
+    except:
+        pass
+    finally:
+        connection.close()
+
+def update_tickets_of_movie():
+    try:
+        connection = sqlite3.connect('cinema.db')
+        cursor = connection.cursor()
+    except:
+        pass
+    finally:
+        connection.close()
+
+def record_ticket_sale():
+    try:
+        connection = sqlite3.connect('cinema.db')
+        cursor = connection.cursor()
+    except:
+        pass
+    finally:
+        connection.close()
+
 while True:
     client, address = server.accept()
     print(f'New connection from address: {address}')
@@ -29,7 +89,7 @@ while True:
     print(f'[RECEIVED] {request}')
     logging.debug(f'Received request from {address}:{request}')
     
-    client.send('Message received.'.encode())
+    client.send('[SENT] Helllo from server!'.encode())
     logging.info(f'Sent response to {address}')
 
     client.close()
